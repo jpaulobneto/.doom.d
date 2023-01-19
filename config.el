306538f534205f63c4213e12c7eb548cffc08c67
João Paulo Barbosa <jpaulobneto@gmail.com>
2023-01-19 11:29:55 -0300

    chore: javascript lsp setup

--- config.el
+++ config.el
@@ -3,6 +3,7 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 (setq projectile-project-search-path '("~/src/" "~/work/"))
+(setq +format-with-lsp nil)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
